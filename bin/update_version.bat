7bbe1c10481e58887050969cf176bd72e21105ab
Warren R. Bank <warren.r.bank@gmail.com>
2025-03-27 04:24:29 -0700

    v4.16.5-neder11ndeu-f56b052.1

--- bin/update_version.bat
+++ bin/update_version.bat
@@ -1,10 +1,14 @@
 @echo off
 
 set new_version=4.16.5
+set new_commit=f56b052cd5055390df789f178911af9eeff03124
+
+rem :: first 7 hex digits
+set new_commit_short=%new_commit:~0,7%
 
 cd /D "%~dp0.."
 
-call node -e "const fs = require('fs'); const pkg = require('./package.json'); pkg.dependencies['@distube/ytdl-core'] = '%new_version%'; pkg.version = '%new_version%-distubejs.1'; fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2), {encoding: 'utf8', flush: true});"
+call node -e "const fs = require('fs'); const pkg = require('./package.json'); pkg.dependencies['@distube/ytdl-core'] = 'github:Neder11ndeu/yt-donglot#%new_commit%'; pkg.version = '%new_version%-neder11ndeu-%new_commit_short%.1'; fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2), {encoding: 'utf8', flush: true});"
 
 call npm install
 
--- bin/update_version.sh
+++ bin/update_version.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 new_version='4.16.5'
+new_commit='f56b052cd5055390df789f178911af9eeff03124'
+
+# first 7 hex digits
+new_commit_short="${new_commit:0:7}"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/.."
 
-node -e "const fs = require('fs'); const pkg = require('./package.json'); pkg.dependencies['@distube/ytdl-core'] = '${new_version}'; pkg.version = '${new_version}-distubejs.1'; fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2), {encoding: 'utf8', flush: true});"
+node -e "const fs = require('fs'); const pkg = require('./package.json'); pkg.dependencies['@distube/ytdl-core'] = 'github:Neder11ndeu/yt-donglot#${new_commit}'; pkg.version = '${new_version}-neder11ndeu-${new_commit_short}.1'; fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2), {encoding: 'utf8', flush: true});"
 
 npm install
 
